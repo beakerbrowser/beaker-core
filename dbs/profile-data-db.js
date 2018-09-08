958d9bc188485bfdd1402be309d181787d50d798
Paul Frazee <pfrazee@gmail.com>
2018-09-08 15:57:54 -0500

    Fix issue with subsequent starts

--- dbs/profile-data-db.js
+++ dbs/profile-data-db.js
@@ -72,7 +72,7 @@ migrations = [
   migration('profile-data.v19.sql'),
   migration('profile-data.v20.sql'),
   migration('profile-data.v21.sql'),
-  migration('profile-data.v22.sql')
+  migration('profile-data.v22.sql', {canFail: true}) // canFail for the same reason as v16, ffs
 ]
 function migration (file, opts = {}) {
   return cb => {
