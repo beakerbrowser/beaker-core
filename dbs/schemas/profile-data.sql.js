471a3f4553eda8bb501719193cdcf32860442dd6
Wesley McCann <septimus98@gmail.com>
2018-10-09 14:40:32 -0600

    Added watchlist to DB
    Bumped PRAGMA version in all related files
    Added to `profile-data.sql.js` for fresh installs

--- dbs/schemas/profile-data.sql.js
+++ dbs/schemas/profile-data.sql.js
@@ -126,6 +126,20 @@ CREATE TABLE apps_log (
   FOREIGN KEY (profileId) REFERENCES profiles (id) ON DELETE CASCADE
 );
 
+-- add a database for watchlist feature
+CREATE TABLE watchlist (
+  profileId INTEGER NOT NULL,
+  url TEXT NOT NULL,
+  description TEXT NOT NULL,
+  seedWhenResolved BOOLEAN NOT NULL,
+  resolved BOOLEAN NOT NULL DEFAULT (0),
+  updatedAt INTEGER DEFAULT (strftime('%s', 'now')),
+  createdAt INTEGER DEFAULT (strftime('%s', 'now')),
+ 
+  PRIMARY KEY (profileId, url),
+  FOREIGN KEY (profileId) REFERENCES profiles (id) ON DELETE CASCADE
+);
+
 -- deprecated
 CREATE TABLE workspaces (
   profileId INTEGER NOT NULL,
@@ -152,5 +166,5 @@ INSERT INTO bookmarks (profileId, title, url, pinned) VALUES (0, 'Report an issu
 INSERT INTO bookmarks (profileId, title, url, pinned) VALUES (0, 'Explore the p2p Web', 'dat://taravancil.com/explore-the-p2p-web.md', 1);
 INSERT INTO bookmarks (profileId, title, url, pinned) VALUES (0, 'Support Beaker', 'https://opencollective.com/beaker', 1);
 
-PRAGMA user_version = 22;
+PRAGMA user_version = 23;
 `
