d42556b24e7560867a09b4601c25aa5b6f4c2114
Paul Frazee <pfrazee@gmail.com>
2019-01-04 13:51:55 -0600

    Split search results into types

--- crawler/search.js
+++ crawler/search.js
@@ -1,6 +1,8 @@
+const _groupBy = require('lodash.groupby')
 const bookmarksDb = require('../dbs/bookmarks')
 const historyDb = require('../dbs/history')
 const datLibrary = require('../dat/library')
+const {getBasicType} = require('../lib/dat')
 
 const BUILTIN_PAGES = [
   {title: 'Timeline', url: 'beaker://timeline'},
@@ -23,23 +25,23 @@ exports.listSuggestions = async function (query = '', opts = {}) {
   // builtin pages
   suggestions.apps = BUILTIN_PAGES.filter(filterFn)
 
-  // bookmarks
-  var bookmarkResults = await bookmarksDb.listBookmarks(0)
-  if (opts.filterPins) {
-    bookmarkResults = bookmarkResults.filter(b => !b.pinned && filterFn(b))
-  } else {
-    bookmarkResults = bookmarkResults.filter(filterFn)
-  }
-  bookmarkResults = bookmarkResults.slice(0, 12)
-  suggestions.bookmarks = bookmarkResults.map(b => ({title: b.title, url: b.href}))
-
   // library
   var libraryResults = await datLibrary.queryArchives({isSaved: true})
   libraryResults = libraryResults.filter(filterFn)
-  suggestions.library = libraryResults.slice(0, 12)
+  Object.assign(suggestions, _groupBy(libraryResults, a => getBasicType(a.type)))
 
-  // fetch history
   if (query) {
+    // bookmarks
+    var bookmarkResults = await bookmarksDb.listBookmarks(0)
+    if (opts.filterPins) {
+      bookmarkResults = bookmarkResults.filter(b => !b.pinned && filterFn(b))
+    } else {
+      bookmarkResults = bookmarkResults.filter(filterFn)
+    }
+    bookmarkResults = bookmarkResults.slice(0, 12)
+    suggestions.bookmarks = bookmarkResults.map(b => ({title: b.title, url: b.href}))
+
+    // history
     var historyResults = await historyDb.search(query)
     suggestions.history = historyResults.slice(0, 12)
     suggestions.history.sort((a, b) => a.url.length - b.url.length) // shorter urls at top
