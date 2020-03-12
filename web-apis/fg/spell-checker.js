48e8f1b52a1f8ec8a2f86ad6c27a24ffda252c61
Paul Frazee <pfrazee@gmail.com>
2020-03-12 11:04:59 -0500

    Remove custom spell-checker and update some deps

--- web-apis/fg/spell-checker.js
+++
@@ -1,6 +0,0 @@
-const spellCheckerManifest = require('../manifests/external/spell-checker')
-
-module.exports = function (rpc) {
-    // create the rpc apis
-    return rpc.importAPI('spell-checker', spellCheckerManifest)
-}
